2e4584d28d65f4793cf9bd1c4cfe91a9853af0ea
Alex Alves <alxac@msn.com>
2025-05-26 14:49:42 -0400

    Conectando no banco

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,19 +1,15 @@
+// connectToDatabase.js
 const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
-    await mongoose.connect(
-        `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cursonodejsdicasparadev.ncg0p.mongodb.net/database?retryWrites=true&w=majority`,
-        (error) => {
-            if (error) {
-                return console.log(
-                    "Ocorreu um erro ao se conectar com o banco de dados: ",
-                    error
-                );
-            }
-
-            return console.log("Conexão ao banco de dados realizada com sucesso!");
-        }
-    );
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@alxac.lcryj.mongodb.net/myFirstDB?retryWrites=true&w=majority`
+        );
+        console.log("Conexão ao banco de dados realizada com sucesso!");
+    } catch (error) {
+        console.error("--#--# Ocorreu um erro ao se conectar com o banco de dados:", error);
+    }
 };
 
 module.exports = connectToDatabase;
